d578ae1e6082eb6fc852d46c8417def3684766a4
Kalle Wallin <kalle.wallin@gmail.com>
2016-09-19 09:50:16 +0200

    Create time-conversion.cs

---
+++ csharp/time-conversion.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+      Console.WriteLine(DateTime.Parse(Console.ReadLine()).ToString("HH:mm:ss"));
+    }
+}
