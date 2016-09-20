a048be80524bcbc4aee8bba6195b7e047540de39
Kalle Wallin <kalle.wallin@gmail.com>
2016-09-20 13:34:10 +0200

    wip

--- csharp/circular-array-rotation.cs
+++ csharp/circular-array-rotation.cs
@@ -1,17 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 class Solution {
     static void Main(String[] args) {
-      var inputnkq = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
+      var input = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
+      var n = input[0] >= 1 ? input[0] <= 100000 ? input[0] : 100000 : 1;
+      var k = input[1] >= 1 ? input[1] <= 100000 ? input[1] : 100000 : 1;
+      var q = input[2] >= 1 ? input[2] <= 500 ? input[2] : 500 : 1;
       var rotArray = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
-      for (var i = 0; i < inputnkq[1]; i++){
-        rotArray = new int[]{rotArray[rotArray.Length-1]}.Concat(rotArray.Take(rotArray.Length-1)).ToArray();
+
+      for (var i = 0; i < k; i++){
+        var tmpArray = new int[n];
+        Array.Copy(rotArray, n-1, tmpArray, 0, 1);
+        Array.Copy(rotArray, 0, tmpArray, 1, n-1);
+        rotArray = tmpArray;
       }
-      for (var i = 0; i < inputnkq[2]; i++){
-        Console.WriteLine(rotArray[Convert.ToInt32(Console.ReadLine())]);
+      
+      for (var i = 0; i < q; i++){
+        var index = Convert.ToInt32(Console.ReadLine());
+        index = index >= 0 ? index < n ? index : n-1 : 0; 
+        Console.WriteLine(rotArray[index]);
       }
     }
 }
